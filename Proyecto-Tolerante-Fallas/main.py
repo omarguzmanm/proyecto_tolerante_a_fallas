10562dee31443b6d026ba036db8d1da596507e3d
Omar Guzmán <85196028+omarguzmanm@users.noreply.github.com>
2022-11-25 15:26:36 -0600

    Add files via upload

--- Proyecto-Tolerante-Fallas/main.py
+++ Proyecto-Tolerante-Fallas/main.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request
 import requests
+import pickle
+
 
 app = Flask(__name__, static_folder='templates')
 
-@app.route('/fotos', methods=['GET','POST'])
+@app.route('/inicio', methods=['GET','POST'])
 def home():
     if request.method == 'GET':
         return render_template('index.html')
@@ -28,6 +30,11 @@ def login():
         return render_template('login.html', data=dataGiphy['data'])
     else:
         return render_template('login.html')
+
+@app.route('/registrarse', methods=['GET','POST'])
+def registro():
+        if request.method == 'GET':
+            return render_template('registrarse.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=4000)
