09757832399415d85070ee1cff2c2a9083b5b718
ferderueda7 <114009509+ferderueda7@users.noreply.github.com>
2022-12-01 23:51:44 -0600

    Add files via upload

---
+++ Proyecto-Tolerante-Fallas/app/conexionBD.py
@@ -0,0 +1,21 @@
+
+#Importando Libreria mysql.connector para conectar Python con MySQL
+import mysql.connector
+
+def connectionBD():
+    mydb = mysql.connector.connect(
+        host ="localhost",
+        port = 8111,
+        user ="root",
+        passwd ="",
+        database = "ejemplo_youtube"
+        )
+    return mydb
+    '''       
+    if mydb:
+        return ("Conexion exitosa")
+    else:
+        return ("Error en la conexion a BD")
+    '''
+    
+    
